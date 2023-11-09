a1d49687abd3b4d56768de56c0d6bfd53b289f1c
Marina <anfina1@yandex.ru>
2023-11-09 08:06:31 +0300

    homework1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_Item_calculate_total_price():
+    test_class = Item('Marina', 100.0, 10)
+    test_data = test_class.calculate_total_price()
+    assert test_data == 1000
+
