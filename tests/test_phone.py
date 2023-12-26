908b954a569e0fac1022799439ef073380d02370
Marina <anfina1@yandex.ru>
2023-12-26 19:31:17 +0300

    переписанный тест

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,36 +1,23 @@
 """Тесты с использованием pytest для модуля phone"""
 
 import pytest
+from src.item import Item
 from src.phone import Phone
 
+@pytest.fixture
+def test_item1():
+    return Item("Смартфон", 10000, 20)
 
-def test___init__():
-    """
-    Тест инициализации
-    """
-    phone1 = Phone("iPhone 14", 120_000, 5, 2)
-    assert str(phone1) == 'iPhone 14'
-
-
-def test_number_of_sim():
-    """
-    Тест количества сим-карт
-    """
-    phone1 = Phone("iPhone 10", 20_000, 5, 2)
-    phone1.number_of_sim = 2
-    assert phone1.number_of_sim == 2
+@pytest.fixture
+def test_phone1():
+    return Phone("iPhone 14", 120_000, 5, 2)
 
 
-def test_number_of_sim__value_error():
-    """
-    Тест с проверкой ошибки малого или отрицательного количества сим-карт
-    """
-    with pytest.raises(ValueError):
-        phone2 = Phone("iPhone 20", 120_000, 5, 0)
-        phone2.number_of_sim = 0
-        phone2.number_of_sim
+def test_name(test_phone1):
+    assert str(test_phone1) == "iPhone 14"
+    assert repr(test_phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+    assert test_phone1.number_of_sim == 2
 
 
-def test___repr__():
-    phone1 = Phone("iPhone 14", 120_000, 5, 2)
-    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+def test_add(test_item1, test_phone1):
+    assert test_phone1 + test_item1 == 25
